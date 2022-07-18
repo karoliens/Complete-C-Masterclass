4fe4e7b7db432ce562d3b4cb12bc8fc5540ebcb9
Karolis <karoliens@gmail.com>
2022-07-18 17:52:39 +0300

    Initial commit

---
+++ Assignment_1_Exercise_Parsing/Assignment_1_Exercise_Parsing/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignment_1_Exercise_Parsing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
